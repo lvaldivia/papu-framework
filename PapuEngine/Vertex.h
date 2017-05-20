9294c89baee09ed7cd67491b3962d7668b1d4e43
Your Name <you@example.com>
2017-05-20 12:40:58 -0500

    adding textures

--- PapuEngine/Vertex.h
+++ PapuEngine/Vertex.h
@@ -13,12 +13,20 @@ struct Color {
 	GLubyte a;
 };
 
+struct UV {
+	float u;
+	float v;
+};
+
 struct Vertex {
 	Position position;
 	Color color;
+	UV uv;
 	
-	
-
+	void setUV(float u, float v) {
+		uv.u = u;
+		uv.v = v;
+	}
 	void setPosition(float x, float y) {
 		position.x = x;
 		position.y = y;
