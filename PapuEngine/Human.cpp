c53a7dc23a72203e0dbd86f3baa24baea44fd70f
Your Name <you@example.com>
2017-06-17 13:33:21 -0500

    human class

--- PapuEngine/Human.cpp
+++ PapuEngine/Human.cpp
@@ -1,18 +1,24 @@
 #include "Human.h"
-
+#include <random>
 
 
 Human::Human()
 {
 }
 
+void Human::init(float speed, glm::vec2 position) {
+	_speed = speed;
+	color.set(185, 0, 0, 255);
+	_position = position;
+
+	static 
+}
 
 void Human::update(const std::vector<std::string>& levelData,
 	std::vector<Human*>& humans,
 	std::vector<Zombie*>& zombies) {
 
 }
-
 
 Human::~Human()
 {
--- PapuEngine/Human.h
+++ PapuEngine/Human.h
@@ -3,9 +3,13 @@
 
 class Human : public Agent
 {
+
+protected:
+	glm::vec2 _direction;
 public:
 	Human();
 	~Human();
+	void init(float speed, glm::vec2 position);
 
 	void update(const std::vector<std::string>& levelData,
 		std::vector<Human*>& humans,
